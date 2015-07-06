6567fd27687e5eacbac6fb4da4e05c2cd7733f39
ethanabrooks <ethanabrooks@gmail.com>
2015-07-06 16:32:45 -0700

    write undominated'

--- src/Main.hs
+++ src/Main.hs
@@ -7,3 +7,9 @@ main = do
     putStrLn "Welcome to FP Haskell Center!"
     putStrLn "Have a good day!"
 
+
+undominated' :: (Ord a) => [(a,a)] -> [(a,a)]
+undominated' [] = []
+undominated' [p] = [p]
+undominated' ps = merge (undominated' left) (undominated' right) 
+    where (left, right) = split ps
