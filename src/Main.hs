c2d008e7d32f08ed5c1f41c5a577dcafec2cd56e
ethanabrooks <ethanabrooks@gmail.com>
2015-07-06 17:12:25 -0700

    working on merge

--- src/Main.hs
+++ src/Main.hs
@@ -7,15 +7,19 @@ main = do
     putStrLn "Welcome to FP Haskell Center!"
     putStrLn "Have a good day!"
 
-compare_x :: (Ord a) => (a,a) -> (a,a) -> Ordering
-compare_x (x1, _) (x2, _)
-    | x1 > x2   = GT
-    | x1 < x2   = LT
-    | otherwise = EQ
+data Dim = X | Y
+
+compareBy :: (Ord a) => Dim -> (a,a) -> (a,a) -> Ordering
+compareBy X (x1, _) (x2, _) = compare x1 x2
+compareBy Y (_, y1) (_, y2) = compare y1 y2
+
+merge left right = [p | p <- left, p above (uppermost right)] ++ right
+    where
+        
 
 median ps = ps !! (length ps) / 2
 
-undominated ps = undominated' sorted where sorted = sortBy compare_x ps
+undominated ps = undominated' sorted where sorted = sortBy (compareBy X) ps
 
 undominated'     :: (Ord a) => [(a,a)] -> [(a,a)]
 undominated' []  = []
